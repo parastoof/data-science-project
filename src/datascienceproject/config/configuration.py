8c0631934bb4cc4930f58c820f010924890e6ed5
Parastoo <prfa9877@gmail.com>
2026-09-13 18:33:18 +0330

    data ingestion pipeline

---
+++ src/datascienceproject/config/configuration.py
@@ -0,0 +1,21 @@
+from src.datascienceproject.constants import *
+from src.datascienceproject.utils.common import read_yaml, create_directories
+from src.datascienceproject.entity.config_entity import DataIngestionconfig
+class ConfiguationManager:
+    def __init__(self, 
+                 config_pathfile=CONFIG_FILE_PATH,
+                 params_pathfile=PARAMS_FILE_PATH,
+                 schema_pathfile=SCHEMA_FILE_PATH):
+        self.config=read_yaml(config_pathfile)
+        self.params=read_yaml(params_pathfile)
+        self.schema=read_yaml(schema_pathfile)
+        create_directories([self.config.artifacts_root])
+
+    def get_data_ingestion_config(self)->DataIngestionconfig:
+        config=self.config.data_ingestion
+        create_directories([config.root_dir])
+        data_ingestion_config=DataIngestionconfig(root_dir=config.root_dir,
+                                                  source_URL=config.source_URL,
+                                                  local_data_file=config.local_data_file,
+                                                  unzip_dir=config.unzip_dir)
+        return data_ingestion_config
